e0ec1927bd643208dd29fe1baad679fd8e39ca3c
dydrlaks <dydrlaks@likelion.org>
2024-11-12 00:45:06 +0900

    feat: Rotate box on frame

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,18 @@
 import * as THREE from "three";
 
-const $result = document.getElementById('result');
-
 // 1. Scene: 화면에서 보여주려는 객체를 담는 공간
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffe287);
 
 // 2. Camera: Scene을 바라볼 시점을 결정
-const camera = new THREE.PerspectiveCamera(50, $result.clientWidth / $result.clientHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(2, 2, 2);
 camera.lookAt(0, 0, 0);
 
 // 3. Renderer: Scene+Camera, 화면을 그려주는 역할
-const renderer = new THREE.WebGLRenderer({
-    canvas: $result,
-    antialias: true,
- });
-renderer.setSize($result.clientWidth, $result.clientHeight);
-// document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const light = new THREE.DirectionalLight(0xffffff);
 light.position.set(2, 4, 3);
@@ -30,4 +25,11 @@ const material = new THREE.MeshStandardMaterial({
 const box = new THREE.Mesh(geometry, material);
 
 scene.add(box);
-renderer.render(scene, camera);
+
+function animate() {
+    box.rotation.y += 0.01;
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+}
+
+animate();
